9d26c94af6ec5d206456dc03a825695e7af59493
S1ro1 <matej.sirovatka@gmail.com>
2023-02-04 01:40:07 +0100

    Change to torchmetrics.Accuracy

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from torch import nn
 from torchvision import transforms
 from torch.utils.data import DataLoader
 import pytorch_lightning as pl
+import torchmetrics
 from pytorch_lightning import loggers as pl_loggers
 from pytorch_lightning.cli import LightningCLI
 from utils import TrafficSignsDataset, get_loader
@@ -16,23 +17,20 @@ class CNN(pl.LightningModule):
 
         self.conv1 = nn.Conv2d(
             in_channels, 64, kernel_size=3, stride=1, padding=1)
-        self.pool1 = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-        self.pool2 = nn.MaxPool2d(2, 2)
-        self.dropout = nn.Dropout(p=0.2)
 
         self.fc1 = nn.Linear(7*7*128, 128, )
         self.fc2 = nn.Linear(128, num_classes)
 
-    def _calculate_accuracy(self, out, y):
-        n_correct = (out.argmax(1) == y).sum().item()
-        accuracy = n_correct/y.shape[0]
+        self.pool = nn.MaxPool2d(2, 2)
+        self.dropout = nn.Dropout(p=0.1)
 
-        return accuracy
+        self.train_accuracy = torchmetrics.Accuracy(task="multiclass", num_classes=43)
+        self.val_accuracy = torchmetrics.Accuracy(task="multiclass", num_classes=43)
 
     def _common_step(self, x, batch_idx):
-        x = self.pool1(self.conv1(x))
-        x = self.pool2(self.conv2(x))
+        x = self.pool(self.conv1(x))
+        x = self.pool(self.conv2(x))
 
         x = x.reshape(x.shape[0], -1)
 
@@ -49,10 +47,10 @@ class CNN(pl.LightningModule):
         out = self._common_step(x, batch_idx)
         loss = F.cross_entropy(out, y)
 
-        accuracy = self._calculate_accuracy(out, y)
+        accuracy = self.train_accuracy(out, y)
 
-        self.log("train accuracy", accuracy, prog_bar=True)
-        self.log("train loss", loss)
+        self.log("train_accuracy", accuracy, prog_bar=True)
+        self.log("train_loss", loss)
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -64,10 +62,9 @@ class CNN(pl.LightningModule):
         out = self._common_step(x, batch_idx)
         loss = F.cross_entropy(out, y)
 
-        accuracy = self._calculate_accuracy(out, y)
+        accuracy = self.val_accuracy.update(out, y)
 
-        self.log("val accuracy", accuracy, prog_bar=True)
-        self.log("val loss", loss)
+        self.log("val_loss", loss, prog_bar=True)
         return loss
 
     def test_step(self, batch, batch_idx):
@@ -79,11 +76,15 @@ class CNN(pl.LightningModule):
         out = self._common_step(x, batch_idx)
         loss = F.cross_entropy(out, y)
 
-        accuracy = self._calculate_accuracy(out, y)
-
-        self.log("test accuracy", accuracy)
-        self.log("test loss", loss)
+        self.log("test loss", loss, prog_bar=True)
         return loss
+
+    def training_epoch_end(self, outputs):
+        self.train_accuracy.reset()
+
+    def validation_epoch_end(self, outputs):
+        self.log('val_accuracy', self.val_accuracy.compute(), prog_bar=True)
+        self.val_accuracy.reset()
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), 3e-4)
@@ -108,6 +109,7 @@ class TrafficDataModule(pl.LightningDataModule):
 
     def val_dataloader(self):
         return self.test_dataloader()
+
 
 if __name__ == "__main__":
     logger = pl_loggers.WandbLogger(
